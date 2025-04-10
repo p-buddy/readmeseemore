fe19506955d73207c40f95cd3944b76f8f06fcc9
p-buddy <parkermalachowsky@gmail.com>
2025-04-10 21:01:27 +0000

    switching to process as communication (instead of websocket)

--- svelte-language-server/src/index.ts
+++ svelte-language-server/src/index.ts
@@ -1,11 +1,9 @@
-import type { Server } from 'node:http';
-import express from 'express';
-import { WebSocketServer } from 'ws';
-import { type IWebSocket, WebSocketMessageReader, WebSocketMessageWriter } from 'vscode-ws-jsonrpc';
+import { StreamMessageReader, StreamMessageWriter } from 'vscode-jsonrpc/node';
 import { createConnection, createServerProcess, forward } from 'vscode-ws-jsonrpc/server';
 import { Message, InitializeRequest, type InitializeParams } from 'vscode-languageserver-protocol';
 import { join } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { name as packageName } from '../package.json';
 
 // see: https://github.com/TypeFox/monaco-languageclient/blob/main/packages/examples/src/common/node/server-commons.ts
 
@@ -13,75 +11,52 @@ let server: ReturnType<typeof createServerProcess>;
 
 const createServer = (name: string) => {
   if (server) return server;
-  const relative = "@readmeseemore/svelte-language-server/dist/index.js"
   const { url } = import.meta;
-  const path = fileURLToPath(url).replace(relative, "");
+  const path = fileURLToPath(url).split(packageName)[0];
   const script = join(path, "svelte-language-server", "bin", "server.js");
   return (server = createServerProcess(name, 'node', [script]));
 }
 
 export const start = (port: number, log = false) => {
   const name = "SvelteLS";
-  const app = express();
-  const server: Server = app.listen(port);
-  const wss = new WebSocketServer({ noServer: true });
-  server.on('upgrade', (request, socket, head) =>
-    wss.handleUpgrade(request, socket, head, webSocket => {
-      const socket: IWebSocket = {
-        send: content => webSocket.send(content, error => {
-          if (log && !error) console.log("sent", content);
-          if (error) throw error;
-        }),
-        onMessage: cb => webSocket.on('message', (data) => {
-          cb(data);
-        }),
-        onError: cb => webSocket.on('error', cb),
-        onClose: cb => webSocket.on('close', cb),
-        dispose: () => webSocket.close()
-      };
 
-      const launch = () => {
-        const reader = new WebSocketMessageReader(socket);
-        const writer = new WebSocketMessageWriter(socket);
-        const socketConnection = createConnection(reader, writer, () => webSocket.terminate());
-        // Spawn the Svelte Language Server process (stdio mode)
-        const serverConnection = createServer(name);
+  const reader = new StreamMessageReader(process.stdin);
+  const writer = new StreamMessageWriter(process.stdout);
+  const dispose = () => {
+    reader.dispose();
+    writer.dispose();
+  }
+  const processConnection = createConnection(reader, writer, dispose);
+  const serverConnection = createServer(name);
 
-        if (!serverConnection) {
-          const msg = 'Failed to spawn Svelte Language Server';
-          console.error(msg);
-          throw new Error(msg);
-        }
+  if (!serverConnection)
+    throw new Error('Failed to spawn Svelte Language Server');
 
-        forward(socketConnection, serverConnection, message => {
-          if (Message.isRequest(message)) {
-            if (message.method === InitializeRequest.type.method) {
-              const initializeParams = message.params as InitializeParams;
-              initializeParams.processId = process.pid;
-            }
-
-            if (log) {
-              console.log(`${name} Server received: ${message.method}`);
-              console.log(message);
-            }
-          }
-
-          if (Message.isResponse(message) && log) {
-            console.log(`${name} Server sent:`);
-            console.log(message);
-          }
-
-          if (Message.isNotification(message) && log) {
-            console.log(`${name} Server notification:`);
-            console.log(message);
-          }
-
-          return message;
-        });
+  forward(processConnection, serverConnection, message => {
+    if (Message.isRequest(message)) {
+      if (message.method === InitializeRequest.type.method) {
+        const initializeParams = message.params as InitializeParams;
+        initializeParams.processId = process.pid;
       }
 
-      if (webSocket.readyState === webSocket.OPEN) launch()
-      else webSocket.on('open', launch);
-    })
-  );
+      if (log) {
+        console.log(`${name} Server received: ${message.method}`);
+        console.log(message);
+      }
+    }
+
+    if (Message.isResponse(message) && log) {
+      console.log(`${name} Server sent:`);
+      console.log(message);
+    }
+
+    if (Message.isNotification(message) && log) {
+      console.log(`${name} Server notification:`);
+      console.log(message);
+    }
+
+    return message;
+  });
+
+  console.log("READY");
 }
