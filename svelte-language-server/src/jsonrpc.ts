cf187366ebb0090902efae95b96e1716b3550d5b
p-buddy <parkermalachowsky@gmail.com>
2025-04-11 22:02:50 +0000

    figured out that like onData was never being called

--- svelte-language-server/src/jsonrpc.ts
+++ svelte-language-server/src/jsonrpc.ts
@@ -1,12 +1,24 @@
-import type { MessageReaderOptions, MessageWriterOptions, ReadableStreamMessageReader, WriteableStreamMessageWriter, } from "vscode-jsonrpc";
+import type { DataCallback, MessageReaderOptions, MessageWriterOptions, ReadableStreamMessageReader, WriteableStreamMessageWriter, Disposable } from "vscode-jsonrpc";
 import { StreamMessageReader as _StreamMessageReader, StreamMessageWriter as _StreamMessageWriter } from "../node_modules/vscode-jsonrpc/lib/node/main";
 import type { Writable, Readable } from 'node:stream';
 
 type Encoding = "ascii" | "utf-8";
 
 export class StreamMessageReader extends _StreamMessageReader implements ReadableStreamMessageReader {
-  constructor(readable: Readable, encoding?: Encoding | MessageReaderOptions) {
-    super(readable, encoding);
+  constructor(private _readable: Readable, encoding?: Encoding | MessageReaderOptions) {
+    super(_readable, encoding);
+  }
+
+  public listen(callback: DataCallback): Disposable {
+    const result = super.listen(callback);
+    const onChunk = (data: Uint8Array) => this["onData"](data);
+    this._readable.on('data', onChunk);
+    return {
+      dispose: () => {
+        result.dispose();
+        this._readable.removeListener('data', onChunk);
+      }
+    };
   }
 }
 
@@ -14,4 +26,6 @@ export class StreamMessageWriter extends _StreamMessageWriter implements Writeab
   constructor(writable: Writable, options?: Encoding | MessageWriterOptions) {
     super(writable, options);
   }
+
+
 }
