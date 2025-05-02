aecb840066863e4de228b86d698154f9966614ca
p-buddy <parkermalachowsky@gmail.com>
2025-05-02 00:02:56 +0000

    adding toolbar component

---
+++ workspace/src/lib/toolbar/Item.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import type { MenuItem } from "./Toolbar.svelte";
+  import Item from "./Item.svelte";
+
+  let { item, depth = 1 }: { item: MenuItem; depth?: number } = $props();
+
+  let open = $state(false);
+</script>
+
+{#snippet right()}
+  <span class="size-4">
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g stroke-width="0"> </g>
+      <g stroke-linecap="round" stroke-linejoin="round"> </g>
+      <g>
+        <path
+          d="M9.71069 18.2929C10.1012 18.6834 10.7344 18.6834 11.1249 18.2929L16.0123 13.4006C16.7927 12.6195 16.7924 11.3537 16.0117 10.5729L11.1213 5.68254C10.7308 5.29202 10.0976 5.29202 9.70708 5.68254C9.31655 6.07307 9.31655 6.70623 9.70708 7.09676L13.8927 11.2824C14.2833 11.6729 14.2833 12.3061 13.8927 12.6966L9.71069 16.8787C9.32016 17.2692 9.32016 17.9023 9.71069 18.2929Z"
+          fill="currentColor"
+        >
+        </path>
+      </g>
+    </svg>
+  </span>
+{/snippet}
+
+<button
+  class="w-full flex text-left items-center justify-between gap-x-3 py-1.5 px-3 rounded-sm text-sm text-neutral-200 disabled:opacity-50 disabled:pointer-events-none focus:outline-none transition-colors duration-100 hover:bg-neutral-600 relative"
+  onmouseenter={() => {
+    open = true;
+  }}
+  onmouseleave={() => {
+    open = false;
+  }}
+  onclick={(e) => {
+    if ("onClick" in item) item.onClick?.();
+    else {
+      e.stopPropagation();
+    }
+  }}
+>
+  <span class="flex-1">
+    {#if typeof item.content === "string"}
+      {item.content}
+    {:else}
+      {@render item.content()}
+    {/if}
+  </span>
+  {#if "children" in item && (item.children?.length ?? 0) > 0}
+    {@render right()}
+    <div
+      class="border-1 border-neutral-600 bg-neutral-800 rounded-sm absolute left-full invisible"
+      style:top={"0"}
+      style:left={"calc(100% - 0.5rem)"}
+      class:visible={open}
+      style:z-index={depth + 1000}
+    >
+      {#if open}
+        {#each item.children as child}
+          <Item item={child} depth={depth + 1} />
+        {/each}
+      {/if}
+    </div>
+  {/if}
+  {#if "shortcut" in item && item.shortcut}
+    <span class="text-neutral-400">{item.shortcut}</span>
+  {/if}
+</button>
---
+++ workspace/src/lib/toolbar/Toolbar.svelte
@@ -0,0 +1,164 @@
+<script lang="ts" module>
+  import { onContextMenu } from "$lib/context-menu/index.js";
+  import { onDestroy, type Snippet } from "svelte";
+  import Item from "./Item.svelte";
+
+  type TextOrSnippet = string | Snippet;
+
+  class State {
+    open = $state(false);
+  }
+
+  type MenuHeader = {
+    content: TextOrSnippet;
+    children: MenuHeader[];
+  };
+
+  export type MenuItem = {
+    content: TextOrSnippet;
+    state?: State;
+  } & (
+    | {
+        shortcut?: string;
+        onClick?: () => void;
+      }
+    | {
+        children: MenuItem[];
+      }
+  );
+
+  const tryInit = (item: MenuItem) => {
+    item.state ??= new State();
+  };
+</script>
+
+<script>
+  // Menu structure with items and their dropdowns
+  const menus: { name: string; items: MenuItem[] }[] = [
+    {
+      name: "File",
+      items: [
+        { content: "New Finder Window", shortcut: "⌘ N" },
+        { content: "New Folder", shortcut: "⇧ ⌘ N" },
+        {
+          content: "New Folder with *Screenshot 2025-05-01 at 12.16.34 PM*",
+          shortcut: "⌥ ⌘ N",
+        },
+        { content: "New Smart Folder", shortcut: "" },
+        { content: "New Tab", shortcut: "⌘ T" },
+        { content: "Open" },
+        {
+          content: "Open With",
+          shortcut: "",
+          children: [
+            { content: "hi" },
+            { content: "hello", children: [{ content: "yes" }] },
+          ],
+        },
+        { content: "Close Window", shortcut: "⌘ W" },
+        { content: "Get Info", shortcut: "⌘ I" },
+        { content: "Rename", shortcut: "" },
+        { content: "Compress", shortcut: "" },
+      ],
+    },
+    {
+      name: "Edit",
+      items: [
+        { content: "Undo", shortcut: "⌘ Z" },
+        { content: "Redo", shortcut: "⇧ ⌘ Z" },
+        { content: "Cut", shortcut: "⌘ X" },
+        { content: "Copy", shortcut: "⌘ C" },
+        { content: "Paste", shortcut: "⌘ V" },
+        { content: "Select All", shortcut: "⌘ A" },
+      ],
+    },
+    {
+      name: "View",
+      items: [
+        { content: "as Icons", shortcut: "⌘ 1" },
+        { content: "as List", shortcut: "⌘ 2" },
+        { content: "as Columns", shortcut: "⌘ 3" },
+        { content: "as Gallery", shortcut: "⌘ 4" },
+      ],
+    },
+    {
+      name: "Go",
+      items: [
+        { content: "Back", shortcut: "⌘ [" },
+        { content: "Forward", shortcut: "⌘ ]" },
+        { content: "Computer", shortcut: "⇧ ⌘ C" },
+        { content: "Home", shortcut: "⇧ ⌘ H" },
+        { content: "Documents", shortcut: "⇧ ⌘ D" },
+      ],
+    },
+    {
+      name: "Window",
+      items: [
+        { content: "Minimize", shortcut: "⌘ M" },
+        { content: "Zoom", shortcut: "" },
+        { content: "Tile Window to Left of Screen", shortcut: "" },
+        { content: "Tile Window to Right of Screen", shortcut: "" },
+      ],
+    },
+    {
+      name: "Help",
+      items: [
+        { content: "Search", shortcut: "" },
+        { content: "Finder Help", shortcut: "⌘ ?" },
+      ],
+    },
+  ];
+
+  let container: HTMLDivElement;
+  let isOpen = $state(false);
+  let index = $state(0);
+
+  const tryHandleClickOutside = ({ target }: MouseEvent) => {
+    if (target instanceof Node && !container.contains(target)) isOpen = false;
+  };
+
+  onDestroy(onContextMenu(tryHandleClickOutside));
+</script>
+
+<svelte:window onclick={tryHandleClickOutside} />
+
+<div
+  bind:this={container}
+  class="w-full text-neutral-200 bg-neutral-700 select-none text-sm font-medium"
+>
+  <div class="menu-bar flex items-center mx-1">
+    <!-- Menu items -->
+    {#each menus as menu, i}
+      {@const open = isOpen && i === index}
+      <button
+        class="text-left relative flex flex-row items-center cursor-default hover:bg-neutral-500 px-3 first:pl-2 gap-2 py-0.5 rounded-sm *:hidden"
+        class:open
+        class:bg-neutral-500={open}
+        onclick={() => {
+          index = i;
+          isOpen = !isOpen;
+        }}
+        onmouseenter={() => {
+          if (!isOpen) return;
+          index = i;
+        }}
+      >
+        {menu.name}
+        <div
+          class="px-1 absolute top-full z-100 left-0 p-1 space-y-0.5 border-1 rounded-sm border-neutral-600 bg-neutral-800"
+          style:min-width={`220px`}
+        >
+          {#each menu.items as item}
+            <Item {item} />
+          {/each}
+        </div>
+      </button>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .open {
+    @apply *:block;
+  }
+</style>
